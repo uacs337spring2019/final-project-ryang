443ef0c3ef3a9d853024d623cee686d54d1be616
Rmg6587 <49927795+Rmg6587@users.noreply.github.com>
2019-04-24 22:06:54 -0700

    Add files via upload

---
+++ Website.js
@@ -0,0 +1,213 @@
+"use strict";
+
+( function () {
+    
+    let type = null;
+    window.onload = function() {
+        document.getElementById("tops-button").onchange = checker;
+        document.getElementById("short-button").onchange = checker;
+        document.getElementById("pants-button").onchange = checker;
+        document.getElementById("access-button").onchange = checker;
+        document.getElementById("shoes-button").onchange = checker;
+        let typesClicker = document.querySelectorAll(".clicker-women");
+                for (let i = 0; i < typesClicker.length; i++) {
+                    typesClicker[i].addEventListener('click', function() {
+                    offProductPage(this.id);
+                    });
+                }
+        let typesClickerMen = document.querySelectorAll(".clicker-men");
+                for (let i = 0; i < typesClickerMen.length; i++) {
+                    typesClickerMen[i].addEventListener('click', function() {
+                    offProductPageMen(this.value);
+                    });
+                }
+        
+        
+        
+    };
+   
+    
+    function populateWomen(type) {
+        let contentArea = document.getElementById("block1-products")
+        contentArea.innerHTML = "";
+        let url = "http://localhost:3000?gen=female&type=" + type;
+        console.log("working");
+        fetch(url)
+            .then(checkStatus)
+            .then(function(responseText) {
+                let resp = JSON.parse(responseText);
+                if (type === "tops") {
+                    let button = document.getElementById("tops-button");
+                    button.checked = true;
+                }else if (type === "skirts") {
+                    let button = document.getElementById("short-button");
+                    button.checked = true;
+                }else if (type === "shoes") {
+                    let button = document.getElementById("shoes-button");
+                    button.checked = true;
+                }else if (type === "pants") {
+                    let button = document.getElementById("pants-button");
+                    button.checked = true;
+                }else if (type === "accessories") {
+                    let button = document.getElementById("access-button");
+                    button.checked = true;
+                }
+                
+                for(let i = 0; i < resp.product.length; i++) {
+                    if(type === resp.product[i].type) {
+                        let productSquare = document.createElement("div");
+                        productSquare.className = "squares";
+                        let img = document.createElement("img");
+                        let desc = document.createElement("h6");
+                        desc.className = "descriptions";
+                        let price = document.createElement("h7");
+                        desc.innerHTML = resp.product[i].description;
+                        price.innerHTML = resp.product[i].price;
+                        
+                        img.className = "productImage";
+                        img.src = "..\\women\\products\\" + resp.product[i].image + ".jpg";
+                        productSquare.appendChild(img);
+                        productSquare.appendChild(desc);
+                        productSquare.appendChild(price);
+                        contentArea.appendChild(productSquare);
+                    }
+                }
+                
+                console.log(resp.product[0].image);
+            })
+       console.log("Yurr");
+        
+    }
+    function populateMen(type) {
+        let contentArea = document.getElementById("block1-products")
+        contentArea.innerHTML = "";
+        let url = "http://localhost:3000?gen=men&type=" + type;
+        console.log("working");
+        fetch(url)
+            .then(checkStatus)
+            .then(function(responseText) {
+                let resp = JSON.parse(responseText);
+                console.log(resp);
+                if (type === "shirts") {
+                    let button = document.getElementById("tops-button");
+                    button.checked = true;
+                }else if (type === "shorts") {
+                    let button = document.getElementById("shorts-button");
+                    button.checked = true;
+                }else if (type === "shoes") {
+                    let button = document.getElementById("shoes-button");
+                    button.checked = true;
+                }else if (type === "pants") {
+                    let button = document.getElementById("pants-button");
+                    button.checked = true;
+                }else if (type === "accessories") {
+                    let button = document.getElementById("access-button");
+                    button.checked = true;
+                }
+                
+                for(let i = 0; i < resp.product.length; i++) {
+                    if(type === resp.product[i].type) {
+                        let productSquare = document.createElement("div");
+                        productSquare.className = "squares";
+                        let img = document.createElement("img");
+                        let desc = document.createElement("h6");
+                        desc.className = "descriptions";
+                        let price = document.createElement("h7");
+                        desc.innerHTML = resp.product[i].description;
+                        price.innerHTML = resp.product[i].price;
+                        
+                        img.className = "productImage";
+                        img.src = "..\\men\\products\\" + resp.product[i].image + ".jpg";
+                        productSquare.appendChild(img);
+                        productSquare.appendChild(desc);
+                        productSquare.appendChild(price);
+                        contentArea.appendChild(productSquare);
+                    }
+                }
+                
+                console.log(resp.product[0].image);
+            })
+       console.log("Yurr");
+        
+    }
+    
+    function offProductPage(value) {
+        document.getElementById("contentAll").innerHTML = "";
+        document.getElementById("content-products-women").style.display = "block";
+        if (value === "category1") {
+            let button = document.getElementById("tops-button");
+            button.checked = true;
+            populateWomen("tops")
+        }else if(value === "category2") {
+            let button = document.getElementById("short-button");
+            button.checked = true;
+            populateWomen("skirts")
+        }else if(value === "category3") {
+            let button = document.getElementById("pants-button");
+            button.checked = true;
+            populateWomen("pants")
+        }else if(value === "category4") {
+            let button = document.getElementById("access-button");
+            button.checked = true;
+            populateWomen("accessories")
+        }else if(value === "category5") {
+            let button = document.getElementById("shoes-button");
+            button.checked = true;
+            populateWomen("shoes")
+        }
+    }
+    function offProductPageMen(value) {
+        document.getElementById("contentAll").innerHTML = "";
+        document.getElementById("content-products-men").style.display = "block";
+        if (value === "category1") {
+            let button = document.getElementById("tops-button");
+            button.checked = true;
+            populateMen("shirts")
+        }else if(value === "category2") {
+            let button = document.getElementById("short-button");
+            button.checked = true;
+            populateMen("shorts")
+        }else if(value === "category3") {
+            let button = document.getElementById("pants-button");
+            button.checked = true;
+            populateMen("pants")
+        }else if(value === "category4") {
+            let button = document.getElementById("access-button");
+            button.checked = true;
+            populateMen("accessories")
+        }else if(value === "category5") {
+            let button = document.getElementById("shoes-button");
+            button.checked = true;
+            populateMen("shoes")
+        }
+    }
+    
+    function checker() {
+        if(this.value === "tops" || this.value === "skirts" || this.value === "pants" || this.value === "shoes" || this.value === "accessories") {
+            populateWomen(this.value);
+        }else if(this.value === "shorts-men") {
+            populateMen("shorts")
+        }else if(this.value === "pants-men") {
+            populateMen("pants")
+        }else if(this.value === "accessories-men") {
+            populateMen("accessories")
+        }else if(this.value === "tops-men") {
+            populateMen("tops")
+        }else if(this.value === "shoes-men") {
+            populateMen("shoes")
+        }
+    }
+    
+    
+    
+    function checkStatus(response) {  
+        if (response.status >= 200 && response.status < 300) {  
+            return response.text();
+        } else if (response.status == 404) {
+            return Promise.reject(new Error("Sorry, we couldn't find that page")); 
+        } else {  
+            return Promise.reject(new Error(response.status+": "+response.statusText)); 
+        } 
+    } 
+    
+})();
---
+++ Website_service.js
@@ -0,0 +1,190 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const fs = require("fs");
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    let gen = req.query.gen;
+    let type = req.query.type;
+    if(gen === "men") {
+        res.send(getMenProduct(type));
+    }else if(gen === "female") {
+        res.send(getWomenProduct(type));
+    }
+    
+
+    
+})
+
+app.listen(3000);
+
+
+function getWomenProduct(type) {
+    let file = fs.readFileSync("women\\products\\productinfo.txt", 'utf8');
+    let lines = file.split("\n");
+    let product = {};
+    let allproducts = [];
+    for(let i = 0; i < lines.length; i++) {
+        if(type === "tops") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "tops") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "skirts") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "skirts") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "pants") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "pants") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "shoes") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "shoes") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "accessories") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "accessories") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "all") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            productEach["image"] = singleline[0];
+            productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+            productEach["description"] = singleline[3];
+            productEach["price"] = singleline[4];
+            allproducts.push(productEach);
+            product["product"] = allproducts;
+        }
+    }
+    return product;
+
+}
+
+function getMenProduct(type) {
+    let file = fs.readFileSync("men\\products\\productinfo.txt", 'utf8');
+    let lines = file.split("\n");
+    let product = {};
+    let allproducts = [];
+    for(let i = 0; i < lines.length; i++) {
+        if(type === "shirts") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "shirts") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "shorts") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "shorts") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "pants") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "pants") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "shoes") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "shoes") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "accessories") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            if (singleline[2] === "accessories") {
+                productEach["image"] = singleline[0];
+                productEach["color"] = singleline[1];
+                productEach["type"] = singleline[2];
+                productEach["description"] = singleline[3];
+                productEach["price"] = singleline[4];
+                allproducts.push(productEach);
+                product["product"] = allproducts;
+            }
+        }else if(type === "all") {
+            let singleline = lines[i].split("::");
+            let productEach = {};
+            productEach["image"] = singleline[0];
+            productEach["color"] = singleline[1];
+            productEach["type"] = singleline[2];
+            productEach["description"] = singleline[3];
+            productEach["price"] = singleline[4];
+            allproducts.push(productEach);
+            product["product"] = allproducts;
+        }
+    }
+    return product;
+
+}
